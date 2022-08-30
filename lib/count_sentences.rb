f3828868ffc8dc34dea0796902e53d79e19ce948
John J Setlock II <johnsetlock@gmail.com>
2022-08-30 11:54:03 -0500

    commit tree 7570d0e8319781711924fcdd9ff3ad53bc3b8539

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    puts self
+    self.end_with?("?")
+
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
+    self.split(/[.|?|!] /).count
 
   end
 end
+
+binding.pry
